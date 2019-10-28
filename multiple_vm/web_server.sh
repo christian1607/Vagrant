80b8b646f30f4ab677a288a19fe5da6989d319b8
christian <christian@christians-MacBook-Pro.local>
2019-10-27 21:15:24 -0500

    fixing order

--- multiple_vm/web_server.sh
+++ multiple_vm/web_server.sh
@@ -1,6 +1,9 @@
 sudo su
-yum update -y
+# Es recomendable tener el os actualizado, comentado solo por ganar tiempo
+#yum update -y
 yum install -y httpd
 sudo su
 service httpd start
+
+echo "Creando index en Server $1"
 echo "Server $1" > /var/www/html/index.html
